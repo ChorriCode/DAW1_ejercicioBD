73b0e93b8857e35230c3d759549cec09ec2cc6cd
Javier <Javier@DRJ>
2018-04-15 19:21:46 +0100

    a ver que pasa

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import controller.Controller;
 import view.View;
 
@@ -7,14 +11,36 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		View myView = new View();
+/*		View myView = new View();
 		Controller myController = new Controller(null, myView);
 
 
 		//Menu eleccion tipo base de datos
 		myView.menuBBDD();
-		myController.controlMenuBBDD(myView);
+		myController.controlMenuBBDD(myView);*/
 
+		
+		
+		
+try {
+			
+			//Registro el driver JDBC
+			Class.forName("org.sqlite.JDBC");		
+			//Abrimos una coneccion a la Base de Datos
+			System.out.println("Connecting to database...");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:ficheros/TiendaInformatica.db");
+			//comprueba si la coneccion es validad y asigno el valor al atributo "validConnection"
+			//del objeto connDB que pertenece a la clase ConnectionDB
+			
+			System.out.println("Database connection ok...");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} 
+	
+		
+		
 		
 	
 	}
